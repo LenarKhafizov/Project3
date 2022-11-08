19a0cb77060f0d501bfbc901d0965f492e412c82
Lenar Khafizov <lenarkh@mail.ru>
2022-11-08 19:57:59 +0300

    Добавление метода T pick()

--- src/MagicBox.java
+++ src/MagicBox.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class MagicBox <T> {
     protected T[] items;
 
@@ -19,6 +21,20 @@ public class MagicBox <T> {
     }
     public T getItem (int itemNumber) {
         return items[itemNumber];
+    }
+
+    public T pick () {
+        for (int i = 0; i < items.length; i++) {
+            if (items[i] == null) {
+                int j = items.length - i;
+                //System.out.println("Коробка не полна, свободных ячеек: " + j);
+                //break;
+                throw new RuntimeException("Коробка не полна, свободных ячеек: " + j);
+            }
+        }
+        Random random = new Random();
+        int randomInt = random.nextInt(items.length);
+        return items[randomInt];
     }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        MagicBox <String> magicBox = new MagicBox<>(2);
-        magicBox.add("Инициализация ");
-        magicBox.add("проекта.");
-        System.out.println(magicBox.getItem(0) + magicBox.getItem(1));
-        boolean operation = magicBox.add(" ");
+        MagicBox <String> magicBox1 = new MagicBox<>(2);
+        magicBox1.add("Инициализация ");
+        magicBox1.add("проекта.");
+        System.out.println(magicBox1.getItem(0) + magicBox1.getItem(1));
+        boolean operation = magicBox1.add(" ");
         System.out.println("Результат операции: " + operation);
+
+        MagicBox <String> magicBox2 = new MagicBox<>(5);
+        System.out.println(magicBox2.pick());
     }
 }
